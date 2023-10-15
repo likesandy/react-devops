c61e5bb09ed0e6be81fafbbb9db706e3bd337393
tao <1445709137@qq.com>
2023-10-15 23:05:28 +0800

    ✨ feat(配置base路径):

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: 'https://react-devops-1303844936.cos.ap-chengdu.myqcloud.com',
 })
 
